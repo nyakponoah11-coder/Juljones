dc0be20eed6fe222f0c6fd28417a13f6997077d8
nyakponoah11-coder <nyakponoah11@gmail.com>
2026-09-10 17:26:51 +0000

    .

--- server.js
+++ server.js
@@ -441,8 +441,9 @@ function generateOrderId() {
   const day = String(now.getDate()).padStart(2, "0");
   const month = String(now.getMonth() + 1).padStart(2, "0");
   const hour = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
 
-  return `JJ-${day}${month}${hour}`;
+  return `JJ-${day}${month}${hour}${minutes}`;
 }
 
 /*--------------------------------------------------------------------------
